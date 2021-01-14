d874d7ddcf3d268f78f332c3d00d67721c00dabc
Alex Wendland <me@alexwendland.com>
2021-01-13 21:51:59 -0800

    feat: improve error reporting on playlist fetch

--- src/api.ts
+++ src/api.ts
@@ -14,56 +14,64 @@ const zip = function* <A, B>(arrA: Array<A>, arrB: Array<B>) {
 }
 
 app.on('get-playlists', async (state: State) => {
-  state.status = 'Fetching playlist info...'
-  app.run('render')
-  console.log('hi1')
+  try {
+    state.status = 'Fetching playlist info...'
+    app.run('render')
 
-  const jsonResp = await fetch(`/.netlify/functions/get-playlists`).then((r) =>
-    r.json()
-  )
-  const playlistResp = jsonResp.playlists
-  console.log('hi3')
-  S.assert(playlistResp, SpotifyPaginatedResponse(SpotifyPlaylist))
-  const concatTracksResp = jsonResp.tracks
-  S.assert(concatTracksResp, S.array(SpotifyPaginatedResponse(SpotifyTrack)))
+    const jsonResp = await fetch(
+      `/.netlify/functions/get-playlists`
+    ).then((r) => r.json())
+    const playlistResp = jsonResp.playlists
+    S.assert(playlistResp, SpotifyPaginatedResponse(SpotifyPlaylist))
+    const concatTracksResp = jsonResp.tracks
+    S.assert(concatTracksResp, S.array(SpotifyPaginatedResponse(SpotifyTrack)))
 
-  state.status = 'Parsing track info...'
-  app.run('render')
+    state.status = 'Parsing track info...'
+    app.run('render')
 
-  state.playlists = playlistResp.items
-  state.allTracks = Array.from(
-    zip(playlistResp.items, concatTracksResp)
-  ).reduce(
-    (allTracks, [playlist, { items: tracks }]) =>
-      allTracks.concat(
-        tracks
-          .sort((t1, t2) => -1 * t1.added_at.localeCompare(t2.added_at)) // Put the newest tracks on top
-          .map(({ track }) => ({
-            name: track.name,
-            spotifyUrl: track.external_urls?.spotify,
-            artists: track.artists.map((a) => a.name),
-            playlist: playlist.name,
-            _searchStr: [
-              track.name,
-              playlist.name,
-              ...track.artists.map((a) => a.name),
-            ]
-              .join(' ')
-              .toLowerCase(),
-          }))
-      ),
-    [] as Array<Track>
-  )
+    state.playlists = playlistResp.items
+    state.allTracks = Array.from(
+      zip(playlistResp.items, concatTracksResp)
+    ).reduce(
+      (allTracks, [playlist, { items: tracks }]) =>
+        allTracks.concat(
+          tracks
+            .sort((t1, t2) => -1 * t1.added_at.localeCompare(t2.added_at)) // Put the newest tracks on top
+            .map(({ track }) => ({
+              name: track.name,
+              spotifyUrl: track.external_urls?.spotify,
+              artists: track.artists.map((a) => a.name),
+              playlist: playlist.name,
+              _searchStr: [
+                track.name,
+                playlist.name,
+                ...track.artists.map((a) => a.name),
+              ]
+                .join(' ')
+                .toLowerCase(),
+            }))
+        ),
+      [] as Array<Track>
+    )
 
-  state.status = `Found ${state.allTracks.length} total tracks in ${state.playlists.length} playlists`
-  app.run('render')
+    state.status = `Found ${state.allTracks.length} total tracks in ${state.playlists.length} playlists`
+    app.run('render')
 
-  // Cache web requests so they don't need to be made on each page load
-  localStorage.setItem(
-    'cached',
-    JSON.stringify({
-      allTracks: state.allTracks,
-      status: state.status,
-    })
-  )
+    // Cache web requests so they don't need to be made on each page load
+    localStorage.setItem(
+      'cached',
+      JSON.stringify({
+        allTracks: state.allTracks,
+        status: state.status,
+      })
+    )
+  } catch (e) {
+    console.error(e)
+    const errorMessage =
+      e instanceof Error
+        ? e.message.replace(/\.?$/, '.') // ensure it ends with a period
+        : 'An error occurred.'
+    state.status = `${errorMessage} Try refreshing?`
+    app.run('render')
+  }
 })
