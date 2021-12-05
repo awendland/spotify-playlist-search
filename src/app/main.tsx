01d03e8fa817950d99b8255a16c3765cf57068df
Alex Wendland <me@alexwendland.com>
2021-12-05 11:21:37 -0500

    feat: focus input when cleared

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -70,6 +70,7 @@ const view = (state: State) => {
             if (inputEl != null) {
               inputEl.value = ''
               app.run('filter', '')
+              inputEl.focus()
             }
           }}
         >
