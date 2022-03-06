7230fb2bd508a56bb39bf91ed449018fb6653bb6
Alex Wendland <me@alexwendland.com>
2022-03-06 10:56:12 -0500

    fix: use aggressive policy to install new service worker

--- webpack.app.js
+++ webpack.app.js
@@ -24,7 +24,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'public/index.html',
     }),
-    new GenerateSW(),
+    new GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
     new CopyPlugin({ patterns: [{ from: 'public/favicons-for-root' }] }),
   ],
   devServer: {
