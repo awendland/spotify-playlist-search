e94abc43c60e231b182779967220e89704383273
Alex Wendland <me@alexwendland.com>
2022-08-13 21:40:39 +0000

    fix: explicitly load assets in service worker since it registers after initial page load

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,19 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(cacheName).then((cache) =>
+    // Populate this list with the key resources required by the app to run.
+    // You can figure out what these are by running Inspecting the page and
+    // watching which network requests are made. Paths are relative to the
+    // origin.
+    cache.addAll([
+      "/",
+      "app.js"
+    ])
+  ))
+})
+
 // Network first, fallback to cache
 self.addEventListener('fetch', (event) => {
   // Open the cache
